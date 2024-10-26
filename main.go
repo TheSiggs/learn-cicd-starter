766449680695e0826daa047b490ca72a9bb44a51
Sam Siggs <sam.siggs@gmail.com>
2024-10-26 21:33:24 +1300

    Fix vulneribilities

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -89,6 +90,7 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
+        ReadHeaderTimeout: 60,
 		Addr:    ":" + port,
 		Handler: router,
 	}
